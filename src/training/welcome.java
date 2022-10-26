aa5c1d778459ba071c1e360af5e73b035b6fdf52
anand <anand@DESKTOP-9JSPD2N.myfiosgateway.com>
2022-10-26 12:29:49 -0400

    first commit

---
+++ src/training/welcome.java
@@ -0,0 +1,11 @@
+package training;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the world of DevOps");
+		System.out.println(20 + 50);
+
+	}
+
+}
