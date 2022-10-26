3f7d1f4aed424f163ad91a41ceb7fbd2497614db
anand <anand@DESKTOP-9JSPD2N.myfiosgateway.com>
2022-10-26 13:24:41 -0400

    second commit

--- src/training/welcome.java
+++ src/training/welcome.java
@@ -5,7 +5,8 @@ public class welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the world of DevOps");
 		System.out.println(20 + 50);
-
+		System.out.println("Program by Sheetal Aruna and Moorti");
+	
 	}
 
 }
